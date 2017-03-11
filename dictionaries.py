965d17e847ed0918cfa89d6fdd67c60e66941041
Robert1GA <roberts.2015@yahoo.co.uk>
2017-03-10 17:46:34 -0800

    lists, dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+# Initialize a dictionary
+food = {'fruit': 'apricot', 'veggie': 'artichoke', 'meat': 'deer', 'water': 'swamp water'}
+print(food)
+
+# To add key value to the dictionary
+food['sweets'] = 'twizzlers'
+
+# To remove key-value from dictionary
+del food['water']
+
+# To get a key-value pair that might not be there (without error)
+# If no key-value with this method, will return None
+# Because trying to access a key-value that doesn't exist would cause
+# an error and program to crash
+result = food.get('grain')
+if result:
+    print(result)
+else:
+    print('key does not exist:', result)
+
+
+print(food)
+# To print the value of a key -
+print(food['meat'])
---
+++ lists.py
@@ -0,0 +1,9 @@
+# List manipulation
+fruits = ['apple', 'banana', 'coconut', 'durian', 'yo-yo', 'zebra']
+# Add a new item to the list with append
+fruits.append('eggplant')
+# Remove an item by name with remove
+fruits.remove('yo-yo')
+# Delete an item by index with del
+del fruits[4]
+print(fruits)
